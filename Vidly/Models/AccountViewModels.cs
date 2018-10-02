ca2468f884a3bfab4bb9e85b650fa50fa3ca417c
young_developman <nazarmazurik99@gmail.com>
2018-10-02 20:40:56 +0300

    Adding progile changes

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -65,6 +65,10 @@ namespace Vidly.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Driver license")]
+        public string DrivingLicense { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
